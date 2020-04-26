b88206c705b6b3c343318bb4b3f8972a37b1207d
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-26 20:59:00 +0000

    Automatically backed up by Learn

---
+++ spec/conference_badges_spec.rb
@@ -0,0 +1,28 @@
+``def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_messages = []
+    array.each do |name|
+    badge_messages << "Hello, my name is #{name}."
+    end
+  badge_messages
+end
+
+def assign_rooms(array)
+  names_with_room_number = []
+  array.each_with_index do |name, index| 
+  names_with_room_number << "Hello, #{name}! You'll be assigned to room #{index + 1}!" 
+  end
+  names_with_room_number
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |list| 
+    puts list
+  end
+  assign_rooms(array).each do |list|
+    puts list
+  end
+end
